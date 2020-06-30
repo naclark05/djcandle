d184c45ddce1f800f23dec0f70a884eaf8731afa
Nikki Clark <nikclarks@protonmail.com>
2020-06-30 16:47:57 -0700

    security

--- candle/settings.py
+++ candle/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from secrets import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'or%f68^az8*$qns$sb2cj!!9x@w)g2iknotie3$6=jhb)-**hc'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
